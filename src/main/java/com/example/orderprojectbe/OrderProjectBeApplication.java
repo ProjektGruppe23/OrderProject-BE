78774b1081fbc225f222152bc0987eba03f5745e
Thea6037 <thea6037@stud.kea.dk>
2023-11-20 13:03:48 +0100

    initial commit

---
+++ src/main/java/com/example/orderprojectbe/OrderProjectBeApplication.java
@@ -0,0 +1,15 @@
+package com.example.orderprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProjectBeApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrderProjectBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orderprojectbe/OrderProjectBeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.orderprojectbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProjectBeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
