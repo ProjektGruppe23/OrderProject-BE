21dc127cee35b25989c8f8f486b5fc63ddd204d8
Thea6037 <thea6037@stud.kea.dk>
2023-11-23 12:57:02 +0100

    added CostumerAddress attributes

--- src/main/java/com/example/orderprojectbe/model/CostumerAddress.java
+++ src/main/java/com/example/orderprojectbe/model/CostumerAddress.java
@@ -1,5 +1,22 @@
 package com.example.orderprojectbe.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class CostumerAddress
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String locality;
+    private String country;
+    private String street_address;
+    private String extended_address;
+    private String postal_code;
+
+    private String name;
+    private String phone;
 }
