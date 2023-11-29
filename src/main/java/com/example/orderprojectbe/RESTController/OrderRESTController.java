401eb5ea770d2d35ffe372383c5ead1db7248ce8
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-11-29 20:10:47 +0100

    Deletemapping

--- src/main/java/com/example/orderprojectbe/RESTController/OrderRESTController.java
+++ src/main/java/com/example/orderprojectbe/RESTController/OrderRESTController.java
@@ -1,18 +1,17 @@
 package com.example.orderprojectbe.RESTController;
 
-import com.example.orderprojectbe.model.CostumerAddress;
 import com.example.orderprojectbe.model.Order;
 import com.example.orderprojectbe.repository.CostumerAddressRepository;
 import com.example.orderprojectbe.repository.OrderRepository;
 import com.example.orderprojectbe.service.reverb.ReverbApiServiceGetAllOrders;
 import com.example.orderprojectbe.service.shopify.ShopifyApiServiceGetAllOrders;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -50,6 +49,31 @@ public class OrderRESTController
     {
         List<Order> lstOrders = orderRepository.findAll();
         return lstOrders;
+    }
+
+    @DeleteMapping("/deleteOrder/{orderId}")
+    public ResponseEntity<String> deleteOrder(@PathVariable int orderId)
+    {
+        try
+        {
+            Optional<Order> optionalOrder = orderRepository.findById(orderId);
+            if ( optionalOrder.isPresent() )
+            {
+                // Order with the specified ID exists, so delete it
+                System.out.println("order with id " + orderId + " deleted successfully");
+                System.out.println("ID: " + orderId + ", Api ID: " + optionalOrder.get().getOrderApiId() + ", Product name: " + optionalOrder.get().getProductName() + ", Quantity: " + optionalOrder.get().getQuantity() + ", Vendor: " + optionalOrder.get().getVendor());
+                orderRepository.deleteById(orderId);
+                return ResponseEntity.ok("Order deleted successfully");
+            }
+            else
+            {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Order not found");
+            }
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while deleting the order");
+        }
     }
 
 }
