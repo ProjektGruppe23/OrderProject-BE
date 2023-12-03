78512ae91df02dd7bb9d0ec7656d5eb6b97b4ddc
Thea6037 <thea6037@stud.kea.dk>
2023-12-03 16:43:35 +0100

    Made ApiService class and moved methods

--- src/main/java/com/example/orderprojectbe/service/shopify/ShopifyApiServiceGetAllOrders.java
+++ src/main/java/com/example/orderprojectbe/service/shopify/ShopifyApiServiceGetAllOrders.java
@@ -1,6 +1,7 @@
 package com.example.orderprojectbe.service.shopify;
 
 import com.example.orderprojectbe.model.Order;
+import com.example.orderprojectbe.service.ApiService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
